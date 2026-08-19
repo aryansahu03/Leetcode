f15f2efda18e1c4b65deaf32c95786b9de1d6363
Aryan Sahu <sahuaryan2729@gmail.com>
2026-08-20 01:35:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (57.71%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int minr = 0;
+        int maxr = m-1;
+        int minc = 0;
+        int maxc = n-1;
+        vector<int> v;
+        while(minr<=maxr && minc<=maxc){
+            // For Right move --> minr row will be constant
+            for(int j=minc ; j<=maxc ;j++){
+                v.push_back(matrix[minr][j]);
+            }
+            minr++;
+            if(minr > maxr || minc > maxc) break;
+            // For down move --> maxc column will be constant
+            for(int i=minr ; i<= maxr ; i++){
+                v.push_back(matrix[i][maxc]);
+            }
+            maxc--;
+            if(minr > maxr || minc > maxc) break;
+            // For left move --> maxr row will be constant
+            for(int j=maxc ; j>= minc ; j--){
+                v.push_back(matrix[maxr][j]);
+            }
+            maxr--;
+            if(minr > maxr || minc > maxc) break;
+            // For up move --> minc column will be constant
+            for(int i=maxr ; i>=minr ; i--){
+                v.push_back(matrix[i][minc]);
+            }
+            minc++;
+            if(minr > maxr || minc > maxc) break;
+        }
+        return v;
+    }
+};
