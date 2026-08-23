cabee63c96bbc70dd46f837b68dfd39c60cd470e
Aryan Sahu <sahuaryan2729@gmail.com>
2026-08-23 22:38:36 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (54.15%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     vector<int> sumZero(int n) {
         vector<int> v(n,0);
-        if(n%2==0){
+        int a = 1;
+        if(n%2!=0) n = n-1;
             for(int i=0 ; i<n ; i=i+2){
-                v[i] = (i+1);
-                v[i+1] = -(i+1);
+                v[i] = a;
+                v[i+1] = -a;
+                a++;
             }
-        }
-        else{
-            for(int i=1 ; i<n ; i=i+2){
-                v[i] = (i+1);
-                v[i+1] = -(i+1);
-            }
-        }
         return v;
     }
 };
